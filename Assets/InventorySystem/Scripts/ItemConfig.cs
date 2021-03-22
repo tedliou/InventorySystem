26c64fd91d977976e159027889e22bf65692b870
Ted Liou <mail@tedliou.com>
2021-03-22 16:14:00 +0800

    建立基本架構、匯入圖片素材
    食物像素圖素材來源：https://henrysoftware.itch.io/pixel-food

---
+++ Assets/InventorySystem/Scripts/ItemConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "InventorySystem/ItemConfig", fileName = "NewItemConfig")]
+public class ItemConfig :ScriptableObject {
+    public new string name;
+    public Sprite defaultImage;
+}
---
+++ Assets/InventorySystem/Scripts/ItemStack.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "InventorySystem/ItemStack", fileName = "NewItemStack")]
+public class ItemStack :ScriptableObject {
+    public List<ItemConfig> itemConfigs;
+}
